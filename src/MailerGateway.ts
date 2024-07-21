f608f0611ca98a305ecf773cae50beb083932c19
Fabiano Vilela <fabiano.vilela@mail.com>
2024-07-21 16:12:51 -0300

    Refactorin applying DDD principles

--- src/MailerGateway.ts
+++
@@ -1,5 +0,0 @@
-export default class MailerGateway {
-  async send(recipient: string, subject: string, message: string): Promise<void> {
-    console.log(recipient, subject, message); // TODO: Remove
-  }
-}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import GetAccount from './GetAccount';
-import Signup from './Signup';
-import AccountController from './AccountController';
-import { AccountRepositoryDatabase } from './AccountRepository';
-import PgPromisseAdapter from './DatabaseConnection';
-import { ExpressAdapter } from './HttpServer';
+import GetAccount from './application/usecases/account/GetAccount';
+import Signup from './application/usecases/account/Signup';
+import AccountController from './infra/controllers/AccountController';
+import { PgPromiseAdapter } from './infra/databases/DatabaseConnection';
+import { ExpressAdapter } from './infra/http/HttpServer';
+import { AccountRepositoryDatabase } from './infra/repositories/AccountRepository';
 
 const httpServer = new ExpressAdapter();
-const databaseConnection = new PgPromisseAdapter();
+const databaseConnection = new PgPromiseAdapter();
 const accountRepository = new AccountRepositoryDatabase(databaseConnection);
 const signup = new Signup(accountRepository);
 const getAccount = new GetAccount(accountRepository);
